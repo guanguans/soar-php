1f415db05d3b5df434bb42be7fde0125f1d8dc62
guanguans <yzmguanguan@gmail.com>
2020-01-20 16:57:06 +0800

    Add PDOExplainAttributes Traits

--- src/PDO.php
+++ src/PDO.php
@@ -12,27 +12,18 @@ declare(strict_types=1);
 
 namespace Guanguans\SoarPHP;
 
-use Guanguans\SoarPHP\Exceptions\InvalidArgumentException;
+use Guanguans\SoarPHP\Traits\PDOExplainAttributes;
 use PDO as BasePDO;
 use PDOException;
 
 class PDO extends BasePDO
 {
+    use PDOExplainAttributes;
+
     /**
      * @var \PDO
      */
     private $conn;
-
-    /**
-     * @var string
-     */
-    private $explainSkeleton = 'EOF
-+----+-------------+---------+------------+------+---------------+------+---------+------+------+----------+-------+
-| id | select_type | table   | partitions | type | possible_keys | key  | key_len | ref  | rows | filtered | Extra |
-+----+-------------+---------+------------+------+---------------+------+---------+------+------+----------+-------+
-| %s |      %s     |    %s   |     %s     |  %s  |       %s      |  %s  |   %s    |  %s  |  %s  |     %s   |   %s  |
-+----+-------------+---------+------------+------+---------------+------+---------+------+------+----------+-------+
-EOF';
 
     /**
      * PDO constructor.
@@ -54,88 +45,6 @@ EOF';
             throw new PDOException($e->getMessage());
         }
         $this->conn = new BasePDO($dsn, $username, $password, $options);
-    }
-
-    /**
-     * @param string $sql
-     *
-     * @return string
-     *
-     * @throws \Guanguans\SoarPHP\Exceptions\InvalidArgumentException
-     */
-    public function getStrExplain(string $sql): string
-    {
-        $explain = $this->getAllExplain($sql);
-
-        return sprintf(
-            $this->getExplainSkeleton(),
-            $explain['id'],
-            $explain['select_type'],
-            $explain['table'],
-            $explain['partitions'],
-            $explain['type'],
-            $explain['possible_keys'],
-            $explain['key'],
-            $explain['key_len'],
-            $explain['ref'],
-            $explain['rows'],
-            $explain['filtered'],
-            $explain['Extra']
-        );
-    }
-
-    /**
-     * @param string $sql
-     *
-     * @return array
-     *
-     * @throws \Guanguans\SoarPHP\Exceptions\InvalidArgumentException
-     */
-    public function getAllExplain(string $sql): array
-    {
-        if (empty($sql)) {
-            throw new PDOException('Sql statement cannot be empty.');
-        }
-
-        return array_merge($this->getExplain($sql, 'partitions'), $this->getExplain($sql, 'extended'));
-    }
-
-    /**
-     * @param string      $sql
-     * @param string|null $type
-     *
-     * @return array
-     *
-     * @throws \Guanguans\SoarPHP\Exceptions\InvalidArgumentException
-     */
-    public function getExplain(string $sql, string $type = null): array
-    {
-        if (null !== $type && !\in_array(\strtolower($type), ['partitions', 'extended'])) {
-            throw new InvalidArgumentException('Invalid type value(partitions/extended): '.$type);
-        }
-        if (false === ($explain = $this->conn->query('EXPLAIN '.$type.' '.$sql, self::FETCH_ASSOC))) {
-            throw new PDOException(sprintf('Sql statement error: %s', $sql));
-        }
-
-        foreach ($explain as $row) {
-            return $row;
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function getExplainSkeleton(): string
-    {
-        return $this->explainSkeleton;
-    }
-
-    /**
-     * @param string $explainSkeleton
-     */
-    public function setExplainSkeleton(string $explainSkeleton)
-    {
-        $this->explainSkeleton = $explainSkeleton;
     }
 
     /**
