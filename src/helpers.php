b376ca8b57c36fccd31a228f0aa31a985984d685
yaozm <ityaozm@gmail.com>
2021-07-07 17:07:44 +0800

    Remove `soar_config` function

--- src/helpers.php
+++ src/helpers.php
@@ -10,38 +10,6 @@ declare(strict_types=1);
  * This source file is subject to the MIT license that is bundled.
  */
 
-use Guanguans\SoarPHP\Config;
-
-if (!function_exists('soar_config')) {
-    /**
-     * @param null $key
-     *
-     * @return \Guanguans\SoarPHP\Config|mixed
-     */
-    function soar_config($key = null)
-    {
-        foreach ([
-                     __DIR__.'/../../../../.soar',
-                     __DIR__.'/../.soar',
-                     __DIR__.'/../../../../.soar.dist',
-                     __DIR__.'/../.soar.dist',
-                 ] as $file) {
-            if (file_exists($file) && is_file($file)) {
-                $config = require $file;
-
-                break;
-            }
-        }
-
-        $config = new Config(isset($config) ? $config : []);
-        if (null === $key) {
-            return $config;
-        }
-
-        return $config->get($key);
-    }
-}
-
 if (!function_exists('array_with_key_reduce')) {
     /**
      * @param null $carry
