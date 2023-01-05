c91924e51cd21df920483e2dd6db02a4a562e64e
guanguans <guanguans@users.noreply.github.com>
2023-01-05 10:26:55 +0000

    Fix styling

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 
 if (! function_exists('array_reduce_with_keys')) {
     /**
-     * @param $carry
-     *
      * @return mixed|null
      */
     function array_reduce_with_keys(array $array, callable $callback, $carry = null)
