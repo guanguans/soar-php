8627fe7112cc5e60646059f7123e11c0d81f970a
yaozm <ityaozm@gmail.com>
2023-01-11 10:21:27 +0800

    Add Args

---
+++ src/Support/Args.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/soar-php.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\SoarPHP\Support;
+
+/**
+ * Escapes a string to be used as a shell argument for Symfony Process.
+ *
+ * This method expects cmd.exe to be started with the /V:ON option, which
+ * enables delayed environment variable expansion using ! as the delimiter.
+ * If this is not the case, any escaped ^^!var^^! will be transformed to
+ * ^!var^! and introduce two unintended carets.
+ *
+ * The file was modified from https://github.com/johnstevenson/winbox-args
+ * MIT Licensed (c) John Stevenson <john-stevenson@blueyonder.co.uk>
+ *
+ * @see https://github.com/johnstevenson/winbox-args
+ * @see https://github.com/composer/composer/blob/main/src/Composer/Util/ProcessExecutor.php
+ */
+class Args
+{
+    /**
+     * Escapes a string to be used as a shell argument.
+     *
+     * Provides a more robust method on Windows than escapeshellarg. When $meta
+     * is true cmd.exe meta-characters will also be escaped. If $module is true,
+     * the argument will be treated as the name of the module (executable) to
+     * be invoked, with an additional check for edge-case characters that cannot
+     * be reliably escaped for cmd.exe. This has no effect if $meta is false.
+     *
+     * Feel free to copy this function, but please keep the following notice:
+     * MIT Licensed (c) John Stevenson <john-stevenson@blueyonder.co.uk>
+     * See https://github.com/johnstevenson/winbox-args for more information.
+     *
+     * @param string|scalar $arg    The argument to be escaped
+     * @param bool          $meta   Additionally escape cmd.exe meta characters
+     * @param bool          $module The argument is the module to invoke
+     *
+     * @psalm-suppress InvalidArgument
+     * @psalm-suppress InvalidScalarArgument
+     *
+     * @return string The escaped argument
+     */
+    public static function escape($arg, bool $meta = true, bool $module = false): string
+    {
+        if (! defined('PHP_WINDOWS_VERSION_BUILD')) {
+            // Escape single-quotes and enclose in single-quotes
+            return "'".str_replace("'", "'\\''", $arg)."'";
+        }
+
+        // Check for whitespace or an empty value
+        $quote = false !== strpbrk($arg, " \t") || '' === (string) $arg;
+
+        // Escape double-quotes and double-up preceding backslashes
+        $arg = preg_replace('/(\\\\*)"/', '$1$1\\"', $arg, -1, $dquotes);
+
+        if ($meta) {
+            // Check for expansion %..% sequences
+            $meta = $dquotes || preg_match('/%[^%]+%/', $arg);
+
+            if (! $meta) {
+                // Check for characters that can be escaped in double-quotes
+                $quote = $quote || false !== strpbrk($arg, '^&|<>()');
+            } elseif ($module && ! $dquotes && $quote) {
+                // Caret-escaping a module name with whitespace will split the
+                // argument, so just quote it and hope there is no expansion
+                $meta = false;
+            }
+        }
+
+        if ($quote) {
+            // Double-up trailing backslashes and enclose in double-quotes
+            $arg = '"'.preg_replace('/(\\\\*)$/', '$1$1', $arg).'"';
+        }
+
+        if ($meta) {
+            // Caret-escape all meta characters
+            $arg = preg_replace('/(["^&|<>()%])/', '^$1', $arg);
+        }
+
+        return $arg;
+    }
+
+    /**
+     * Escapes an array of arguments that make up a shell command.
+     *
+     * The first argument must be the module (executable) to be invoked.
+     *
+     * @param array<string|scalar> $args A list of arguments, with the module name first
+     * @param bool                 $meta Additionally escape cmd.exe meta characters
+     *
+     * @return string The escaped command line
+     */
+    public static function escapeCommand(array $args, bool $meta = true): string
+    {
+        $cmd = self::escape(array_shift($args), $meta, true);
+        foreach ($args as $arg) {
+            $cmd .= ' '.self::escape($arg, $meta);
+        }
+
+        return $cmd;
+    }
+}
