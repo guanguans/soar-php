4de799b71eb3b7e54467c7dd1befc7bdfc6337d0
yaozm <ityaozm@gmail.com>
2024-01-29 15:03:53 +0800

    fix(HasSudoPassword): Update shouldApplySudoPassword condition
    - Update the condition in shouldApplySudoPassword method to include 'embed' in the PHP_SAPI check

--- src/Concerns/HasSudoPassword.php
+++ src/Concerns/HasSudoPassword.php
@@ -41,6 +41,6 @@ trait HasSudoPassword
 
     protected function shouldApplySudoPassword(): bool
     {
-        return $this->sudoPassword && OS::isUnix() && ! \in_array(\PHP_SAPI, ['cli', 'cli-server', 'phpdbg'], true);
+        return $this->sudoPassword && OS::isUnix() && ! \in_array(\PHP_SAPI, ['cli', 'cli-server', 'phpdbg', 'embed'], true);
     }
 }
