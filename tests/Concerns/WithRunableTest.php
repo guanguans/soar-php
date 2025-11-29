94ee75b9a30b380226adad288698f483527dac37
guanguans <ityaozm@gmail.com>
2025-11-29 21:55:51 +0800

    test(WithRunableTest): Skip tests on macOS
    - Change skip() to skipOnMac() for better platform-specific handling.
    - Ensures tests are appropriately skipped when running on macOS.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- tests/Concerns/WithRunableTest.php
+++ tests/Concerns/WithRunableTest.php
@@ -56,7 +56,7 @@ it('will throw ProcessFailedException when sudo password is error', function ():
         'sudo: no password was provided',
         'sudo: 1 incorrect password attempt',
     ]))
-    ->skip()
+    ->skipOnMac()
     ->skip(running_in_github_action());
 
 it('can run soar process with pipe', function (): void {
